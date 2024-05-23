2e220cfd5d7ab4c9fb4c94946e4b836e73bca025
iconsWork <iconstanzav1@gmail.com>
2024-05-23 11:16:27 -0400

    first commit

---
+++ tutorial.php
@@ -0,0 +1,13 @@
+<?php
+
+//HolaMundo
+echo "Hola, PHP \n"; //\n salto de linea
+
+//variable  // tipado dinamico
+$my_string = "Esto es una cadena de texto";
+$my_string = "Aqui cambio el valor de la cadena de texto";
+echo $my_string . "\n"; //imprimio salto de linea
+echo gettype($my_string) . "\n"; //imprimio string
+
+$my_string = 6; //imprimio 6
+echo $my_string . "\n"; //imprimio salto de linea
